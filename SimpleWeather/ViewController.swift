e7bb1bb0e766d0d8ef2286b07efd7a445ba54360
Pavel Parshin <mail@pavelparshin.ru>
2020-06-23 16:07:32 +0300

    Initial Commit

---
+++ SimpleWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by Pavel Parshin on 23.06.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
